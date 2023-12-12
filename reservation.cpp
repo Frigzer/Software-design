7eee5b09d2dfaea72feb368bba3edbc8ff286eec
pbaranski03 <120450563+pbaranski03@users.noreply.github.com>
2023-12-12 10:31:24 +0100

    Add files via upload

---
+++ reservation.cpp
@@ -0,0 +1,11 @@
+#include "reservation.h"
+
+void reservation::ConfirmReservation() {
+	// TODO - implement reservation::ConfirmReservation
+	throw "Not yet implemented";
+}
+
+void reservation::Edit() {
+	// TODO - implement reservation::Edit
+	throw "Not yet implemented";
+}
