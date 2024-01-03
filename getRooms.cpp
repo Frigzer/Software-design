16eda08d721cee2d29bb839934c41689154cd54c
pbaranski03 <pbaranski0316@gmail.com>
2024-01-03 17:59:43 +0100

    rooms from file

--- getRooms.cpp
+++ getRooms.cpp
@@ -1,1 +1,71 @@
+#include "getRooms.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+void rooms::getFromFile()
+{
+    string contents, line;
+    fstream file;
+    file.open("rooms.txt");
+    if(file.is_open())
+    {
+        while(std::getline(file, line))
+        {
+            int flag;
+            int o;
+            char c;
+            bool s;
+            string num;
+            for(int i=0; i<line.size(); i++)
+            {
+                if(line[i]=='/')
+                {
+                    flag++;
+                }
+                switch(flag)
+                {
+                    case 0:
+                    num+=line[i];
+                    break;
+                    case 1:
+                    if(isalnum(line[i]))
+                    {
+                         o=line[i];
+                    }
+                    break;
+                    case 2:
+                    if(isalnum(line[i]))
+                    {
+                         c=line[i];
+                    }
+                    break;
+                    case 3:
+                    if(isalnum(line[i]))
+                    {
+                         if(line[i]=='Y')
+                         {
+                            s=true;
+                         }
+                         else if(line[i]=='N')
+                         {
+                            s=false;
+                         }
+                    }
+                    break;
 
+                }
+                rooms* newRoom = new rooms;
+                newRoom->roomNumber=stoi(num);
+                newRoom->occupants=o;
+                newRoom->roomClass=c;
+                newRoom->isForService=s;
+                Rooms.push_back(newRoom);
+            }
+        }
+        for(int i = 0; i<Rooms.size(); i++)
+        {
+            cout<<Rooms[i].roomNumber<<" - ";
+        }
+    }
+
+}
--- getRooms.h
+++ getRooms.h
@@ -1,1 +1,18 @@
+#ifndef GETROOMS_H
+#define GETROOMS_H
+#include <vector>
+using namespace std;
 
+vector<rooms> Rooms;
+
+struct rooms {
+    int roomNumber;
+    int occupants;
+    char roomClass;
+    bool isForService;
+
+	void getFromFile();
+    void modifyRooms();
+};
+
+#endif
