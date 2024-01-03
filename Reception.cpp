8427b5c98c41b254e4251091d28ad9bccf54f913
Frigzer <borekadam89@gmail.com>
2024-01-03 14:19:21 +0100

    Recepcja - rezerwacja, zarys (napewno ten wrapper jest potrzebny? xD wystarczy chyba jakiś konstruktor)

--- Reception.cpp
+++ Reception.cpp
@@ -1,11 +1,19 @@
 #include "Reception.h"
 
-void Reception::editReservation() {
-	// TODO - implement Reception::editReservation
+void Reception::editReservation(int roomNumber, int task) {
+
+	std::cout << "What would you like to do? \nCreate new reservation - 1 \nEdit reservation - 2" << std::endl;
+	if (task == 1)
+		//Create reservation
+	else if (task == 2)
+		//Edit existing one
+	else
+		std::cout << "Wrong task number\n";
 	throw "Not yet implemented";
 }
 
 void Reception::editWebsite() {
+
 	// TODO - implement Reception::editWebsite
 	throw "Not yet implemented";
 }
--- Reception.h
+++ Reception.h
@@ -1,11 +1,17 @@
 #ifndef RECEPTION_H
 #define RECEPTION_H
 
+#include <iostream>
+#include <vector>
+#include "reservationWrapper.h"
+
 class Reception : User {
 
 
+	std::vector<reservationWrapper*> worker;
+
 public:
-	void editReservation();
+	void editReservation(int roomNumber, int task);
 
 	void editWebsite();
 
