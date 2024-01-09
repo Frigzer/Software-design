825d4b8dc12d5cedc2a1071fc8f36fb6330b4108
pbaranski03 <120450563+pbaranski03@users.noreply.github.com>
2024-01-09 04:44:03 +0100

    Update MakeReservation.h

--- MakeReservation.h
+++ MakeReservation.h
@@ -1,7 +1,14 @@
 #ifndef MAKERESERVATION_H
 #define MAKERESERVATION_H
+#include <vector>
+#include <string>
 
-class MakeReservation {
-};
+using namespace std;
+int generateID(int roomNum, int checkIn);
+vector<rooms*> getFreeRooms(int checkIn, int checkOut);
+int guestsNumber(vector<rooms*> roomNumbers);
+void allAccesibleDates(int checkIn, int checkOut, int numOfGuests);
+void makeReservation();
+void editReservation();
 
 #endif
