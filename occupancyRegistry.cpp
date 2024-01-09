55a7f1604b37bc0a086b80f092225ad7119fce28
pbaranski03 <120450563+pbaranski03@users.noreply.github.com>
2024-01-09 04:24:21 +0100

    Update occupancyRegistry.cpp

--- occupancyRegistry.cpp
+++ occupancyRegistry.cpp
@@ -7,123 +7,127 @@
 
 bool compare(occupancy* o1, occupancy* o2)
 {
-    return(o1->date < o2->date);
+	return(o1->date < o2->date);
+}
+
+void occupancy::writeToFile()
+{
+	sort(OCCUPANCY.begin(), OCCUPANCY.end(), compare);
+	fstream file;
+	file.open("OCCUPANCY.txt", ios::out | ios::trunc);
+	string text;
+	for (int i = 0; i < OCCUPANCY.size(); i++)
+	{
+		bool flag = 0;
+		for (int j = 0; j < OCCUPANCY[i]->roomNumber.size(); j++)
+		{
+			if (flag == 1)
+			{
+				text += "/";
+			}
+			flag = 1;
+			if (OCCUPANCY[i]->isOccupied[j] == true)
+			{
+				text += "!";
+			}
+
+			text += to_string(OCCUPANCY[i]->roomNumber[j]);
+		}
+		text += "|";
+		text += to_string(OCCUPANCY[i]->date);
+		text += "\n";
+	}
+	file << text;
+	file.close();
 }
 int occupancy::numOfDays(int checkIn, int checkOut)
 {
 	readFromFile();
-	int days;
+	int days=0;
 	for (int i = 0; i < OCCUPANCY.size(); i++)
 	{
-		if (OCCUPANCY[i]->date >= checkIn && OCCUPANCY[i]->date <= checkOut)
+		if (OCCUPANCY[i]->date >= checkIn && OCCUPANCY[i]->date < checkOut)
 		{
 			days++;
 		}
 	}
 	return days;
 }
-void occupancy::writeToFile()
-{
-    sort(OCCUPANCY.begin(), OCCUPANCY.end(), compare);
-    fstream file;
-    file.open("OCCUPANCY.txt", ios::out | ios::trunc);
-    string text;
-    for (int i = 0; i < OCCUPANCY.size(); i++)
-    {
-        bool flag = 0;
-        for (int j = 0; j < OCCUPANCY[i]->roomNumber.size(); j++)
-        {
-            if (flag == 1)
-            {
-                text += "/";
-            }
-            flag = 1;
-            if (OCCUPANCY[i]->isOccupied[j] == true)
-            {
-                text += "!";
-            }
-
-            text += to_string(OCCUPANCY[i]->roomNumber[j]);
-        }
-        text += "|";
-        text += to_string(OCCUPANCY[i]->date);
-        text += "\n";
-    }
-    file << text;
-    file.close();
-}
-
 void occupancy::readFromFile()
 {
-    OCCUPANCY.clear();
-    string contents, line;
-    fstream file;
-    file.open("OCCUPANCY.txt");
-    if (file.is_open())
-    {
-        while (getline(file, line))
-        {
-            string num;
-            string date;
-            bool occupied = false;
-            bool flag = false;
-            occupancy* newNode = new occupancy;
-            for (int i = 0; i < line.size(); i++)
-            {
-                if (line[i] == '|')
-                {
-                    flag = true;
-                    newNode->roomNumber.push_back(stoi(num));
-                    num = "";
-                    if (occupied == true)
-                    {
-                        newNode->isOccupied.push_back(true);
-                        occupied = false;
-                    }
-                    else
-                    {
-                        newNode->isOccupied.push_back(false);
-                    }
-                }
-                if (flag == true && isdigit(line[i]))
-                {
-                    date += line[i];
-                }
-                else
-                {
-                    if (line[i] == '!')
-                    {
-                        occupied = true;
-                    }
-                    if (line[i] == '/')
-                    {
-                        newNode->roomNumber.push_back(stoi(num));
-                        num = "";
-                        if (occupied == true)
-                        {
-                            newNode->isOccupied.push_back(true);
-                            occupied = false;
-                        }
-                        else
-                        {
-                            newNode->isOccupied.push_back(false);
-                        }
+	OCCUPANCY.clear();
+	string contents, line;
+	fstream file;
+	file.open("OCCUPANCY.txt");
+	if (file.is_open())
+	{
+		while (getline(file, line))
+		{
+			string num;
+			string date;
+			bool occupied = false;
+			bool flag = false;
+			occupancy* newNode = new occupancy;
+			for (int i = 0; i < line.size(); i++)
+			{
+				if (line[i] == '|')
+				{
+					flag = true;
+					newNode->roomNumber.push_back(stoi(num));
+					num = "";
+					if (occupied == true)
+					{
+						newNode->isOccupied.push_back(true);
+						occupied = false;
+					}
+					else
+					{
+						newNode->isOccupied.push_back(false);
+					}
+				}
+				if (flag == true && isdigit(line[i]))
+				{
+					date += line[i];
+				}
+				else
+				{
+					if (line[i] == '!')
+					{
+						occupied = true;
+					}
+					if (line[i] == '/')
+					{
 
-                    }
-                    if (isdigit(line[i]))
-                    {
-                        num += line[i];
+						newNode->roomNumber.push_back(stoi(num));
+						num = "";
+						if (occupied == true)
+						{
+							newNode->isOccupied.push_back(true);
+							occupied = false;
+						}
+						else
+						{
+							newNode->isOccupied.push_back(false);
+						}
 
-                    }
-                }
+					}
+					if (isdigit(line[i]))
+					{
+						num += line[i];
 
-            }
-            newNode->date = stoi(date);
-            OCCUPANCY.push_back(newNode);
+					}
+				}
 
-        }
-    }
-    file.close();
+			}
+			if (isdigit(date[0]))
+			{
+					newNode->date = stoi(date);
+			}
+			OCCUPANCY.push_back(newNode);
+
+		}
+	}
+	file.close();
 
 }
 
@@ -133,7 +137,7 @@ void occupancy::modifyRegistry(int checkIn, int checkOut, vector<int>rooms, bool
 	int index;
 	index = (checkIn - OCCUPANCY[0]->date);
 	int index2 = numOfDays(checkIn, checkOut) + index;
-	for (int i = index; i <= index2; i++)
+	for (int i = index; i < index2; i++)
 	{
 		for (int j = 0; j < rooms.size(); j++)
 		{
@@ -153,7 +157,6 @@ void occupancy::modifyRegistry(int checkIn, int checkOut, vector<int>rooms, bool
 	}
 	writeToFile();
 }
-
 
 bool occupancy::checkIfFree(int checkIn, int checkOut, vector<int> rooms)
 {
@@ -176,20 +179,4 @@ bool occupancy::checkIfFree(int checkIn, int checkOut, vector<int> rooms)
 		}
 	}
 	return true;
-}
-
-void print()
-{
-    for (int i = 0; i < OCCUPANCY.size(); i++)
-    {
-        for (int j = 0; j < OCCUPANCY[i]->roomNumber.size(); j++)
-        {
-            if (OCCUPANCY[i]->isOccupied[j] == true)
-            {
-                cout << "!";
-            }
-            cout << OCCUPANCY[i]->roomNumber[j] << "/";
-        }
-        cout << "|" << OCCUPANCY[i]->date << endl;
-    }
 }
