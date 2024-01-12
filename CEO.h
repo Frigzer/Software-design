e293a7b5fdf5fe94464765215cceb52cad5fe281
Frigzer <borekadam89@gmail.com>
2024-01-12 16:02:03 +0100

    ceo

--- CEO.h
+++ CEO.h
@@ -1,28 +1,14 @@
 #ifndef CEO_H
 #define CEO_H
 
-#include <iostream>
-#include <fstream>
-#include <ctime>
-#include <vector>
-#include <sstream>
-#include <iomanip>
-
-
 #include "Menager.h"
+#include "predictPrices.h"
 
 class CEO : public Menager {
 
-	// Funkcja wczytuj¹ca daty œwi¹t z pliku
-	std::vector<std::tm> readHolidays();
-	// Funkcja sprawdzaj¹ca, czy data jest œwiêtem
-	bool isHoliday(const std::tm& date, const std::vector<std::tm>& holidays);
-
 public:
-	//Ustalanie ceny na podstawie podanej daty
-	int setPrices(const std::string& dateStr, std::string roomType);
+	
+	void setPrices();
 };
-
-static CEO ceo;
 
 #endif
