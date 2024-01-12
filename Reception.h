7c1156bf4e71804e8d1678d878fa19f838d3715d
Frigzer <borekadam89@gmail.com>
2024-01-12 16:02:35 +0100

    reception

--- Reception.h
+++ Reception.h
@@ -1,14 +1,11 @@
 #ifndef RECEPTION_H
 #define RECEPTION_H
 
-#include <iostream>
 #include <vector>
 
 #include "User.h"
 #include "occupancyRegistry.h"
 #include "websiteData.h"
-#include "MakeReservation.h"
-#include "reservation.h"
 
 class Reception : public User {
 
