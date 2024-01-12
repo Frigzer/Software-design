7f6b99ed1e614f9e4c6991458ae48c20fe40571b
Frigzer <borekadam89@gmail.com>
2024-01-12 16:02:43 +0100

    user

--- User.cpp
+++ User.cpp
@@ -1,6 +1,6 @@
 #include "User.h"
 
-bool User::login() {
+bool User::login(string role) {
 
 	// Logowanie do systemu na podstawie podanej nazwy
 	std::cout << "Type your name: "; std::cin >> username;
@@ -19,10 +19,10 @@ bool User::login() {
     std::string line;
     while (std::getline(file, line)) {
         std::istringstream iss(line);
-        std::string stored_username, stored_password;
+        std::string stored_username, stored_password, stored_role;
 
-        if (iss >> stored_username >> stored_password) {
-            if (stored_username == username && stored_password == password) {
+        if (iss >> stored_username >> stored_password >> stored_role) {
+            if (stored_username == username && stored_password == password && stored_role == role) {
                 std::cout << "Succes" << std::endl;
                 file.close();
 
